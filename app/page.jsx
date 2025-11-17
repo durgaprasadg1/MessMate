fd1de8cf5f4f40349aef3addd3e3659f75bd26c0
Durgaprasad <durgaprasadgirepunje@gmail.com>
2025-11-17 14:41:48 +0530

    Added More btns to AdMin Side

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import Navbar from "@/Component/Others/Navbar";
 import Footer from "@/Component/Others/Footer";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
