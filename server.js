82c4dc2b30385a6afbf7ec2089c905fc19127c56
Durgaprasad <durgaprasadgirepunje@gmail.com>
2026-03-31 14:03:51 +0530

    Final Fixes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import next from "next";
 import { initSocketServer } from "./lib/socket.js";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = dev ? "localhost" : "0.0.0.0";
 const port = process.env.PORT || 3000;
 
 const app = next({ dev, hostname, port });
